45e0612c9dbce3ac6245a6f53f49491de6dd33d7
Bobak <bth5032@gmail.com>
2016-06-02 17:11:14 +0100

    Added ability to iterate through the configurations in your file with loadNextConfig. Fixed bugs with EOF.

--- ConfigParser.C
+++ ConfigParser.C
@@ -15,21 +15,42 @@ bool isWhiteSpace(string str){
 	return true;
 }
 
+bool hasKey(map<string, string> m, string key){
+	//returns true if the map has the key, false otherwise.
+	map<string, string>::iterator i = m.find(key);
+	if (i != m.end()){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
 class ConfigParser{
 
 private:
 	ifstream *config_file;
 	map<string, string> options;
+	map<string, string> default_options;
+	int currentLocation=0;
 
-	void extractOptFromLine(string line){
+	void extractOptFromLine(string line, bool default_opt=false){
 		/*Extract a value from a line*/
-		string opt_key;
-		string opt_value;
+		string opt_key; // holds key from line
+		string opt_value; // holds value for key
 		opt_key=line.substr(0, line.find('='));
 		opt_value=line.substr(line.find('=')+1);
+		
 		if(opt_key != "" && opt_value != ""){
-			options[opt_key] = opt_value;
+			//if default option, add it to defaults dict
+			if (default_opt){
+				default_options[opt_key] = opt_value;	
+			} 
+			else{
+				options[opt_key] = opt_value;
+			}
 		}
+		
 		else{
 			cout<<"Error parsing line number: "<<config_file->tellg()<<" : "<<line<<endl;
 		}
@@ -39,12 +60,57 @@ private:
 public:
 
 	ConfigParser(string filename){
+		//just opens the file stream 
 		config_file = new ifstream(filename);
 	}
 
+	string findFirstConfig(){
+		config_file->seekg(0,config_file->beg); //set to begining
+		return findNextConfig();
+	}
+
+	string findNextConfig(){
+		//Finds and returns name token in next line with "Name="
+		string line;  //holds line of config file
+		while(getline(*config_file, line)){
+			if (line.front() == '#') continue; 
+			if (line.substr(0,5) == "Name="){
+				return line.substr(5);
+			}
+		}
+		return "";
+	}
+
+	bool loadNextConfig(){
+		//loads the next configuration, will load the first one if called before any other config.
+		config_file->seekg(currentLocation);
+		string next_name=findNextConfig();
+		if ( next_name != ""){
+			return loadConfig(next_name);
+		}
+		else{
+			return false;
+		} 
+	}
+
 	bool loadConfig(string config_name){
-		bool found_config=false;
-		options.clear();
+		//
+		//	Starts from the begining of the file and picks up all the defaults until it gets to 
+		//  the config_name shown, and then marks the location in currentLocation. If it can not 
+		//  find the configuration, it will return false, the currentLocation will be set to 0, and it will
+		//  completely clear the defualts and options. Otherwise it will return true and set all the 
+		//  defaults from the begining of the file until the next "Name=" line, blank space, or 
+		//  the end of the file, overwriting defaults with the same name. No attempt is made to 
+		//  put the file in a specific location after the configuration has been loaded, unless the EOF.
+		//  is reached, at which point the file is set back to the beginning. 
+		//
+
+		bool found_config=false; //lets the user know whether we actually found a match. 
+		config_file->clear();
+		config_file->seekg(0,config_file->beg); //always start from the begining of the file to pick up defaults, will slow down everything some amount but whatever.
+		
+		options.clear(); // clear all the non-default options
+		
 		string line; //Holds arbitrary lines.
 		string opt_line; //Holds lines like option="value".
 		string opt_key; //Holds 'option' in the above example
@@ -58,50 +124,70 @@ public:
 			if (line.substr(0, 9) == "DEFAULT::"){
 				opt_line=line.substr(9);
 				//cout<<"Found default opt: "<<opt_line<<endl;
-				extractOptFromLine(opt_line);
+				extractOptFromLine(opt_line, true); //if we find an option this way, add it to the defaults map. 
 			}
 
 			else if (line == "Name="+config_name){
+				currentLocation=config_file->tellg();
 				found_config=true;
-				extractOptFromLine(line);
+				extractOptFromLine(line); // should extract the name
 				getline(*config_file, line);
+				//get lines while we don't have white space or a new config.
 				while( ! ( isWhiteSpace(line) || (line.substr(0,5) == "Name=" ) ) ){
 					//cout<<"SECOND LOOP"<<line<<endl;
 					extractOptFromLine(line);
 					if (config_file->eof()){
-						break;
+						//If we're at the end of the file, getline returns the last line again, but also sets EOF. 
+						break; //break out of config filling loop if EOF is reached 
 					}
-					getline(*config_file, line);
+					getline(*config_file, line); 
 				}
-				config_file->seekg(0,config_file->beg);
-				break;
+				break; //break out of looking for configs/defaults if we've already loaded a config
 			}
 		}
-		
-		config_file->seekg(0,config_file->beg);
-		
+
 
 		if (! found_config){
 			cout<<"configuration not found!"<<endl;
 			options.clear();
-			return false;
+			currentLocation=0;
+			default_options.clear();
 		}
 
-		return true;
+		return found_config;
 	}
 
 	void print(){
+		for (std::map<string,string>::iterator it=default_options.begin(); it!=default_options.end(); it++){
+	    	cout <<"DEFAULT::"<< it->first << " => " << it->second << endl;
+		}
 		for (std::map<string,string>::iterator it=options.begin(); it!=options.end(); it++){
 	    	cout << it->first << " => " << it->second << endl;
 		}
 	}
 
 	string get(string key){
-		return options[key];
+		if (hasKey(default_options, key)) {
+			return default_options[key];
+		}
+		else if (hasKey(options, key)){
+			return options[key];
+		}
+		else{
+			return "";
+		}
 	}
 
-	string operator [](string key) {
-		return options[key];
+	string operator [] (string key) {
+		if (hasKey(default_options, key)) {
+			return default_options[key];
+		}
+		else if (hasKey(options, key)){
+			return options[key];
+		}
+		else{
+			return "";
+		}
 	}
 
 };
--- test.C
+++ test.C
@@ -1,7 +1,7 @@
 #include "ConfigParser.C"
 #include <string>
 
-void test(){
+void testRandomAccess(){
 	ConfigParser conf("test.conf");
 	
 	cout<<"==================\n";
@@ -15,8 +15,49 @@ void test(){
 	conf.print();
 	cout<<"==================\n\n";
 
+	cout<<"==================\n";
+	cout<<"Loading Configuration for Name=80_vtx"<<endl;
+	conf.loadConfig("80_vtx");
+	conf.print();
+	cout<<"==================\n\n";
+
+	cout<<"Loading Configuration for Name=76"<<endl;
+	conf.loadConfig("76");
+	conf.print();
+	cout<<"==================\n\n";
+
 	cout<<"Loading Configuration for Name=80_vtx_mf"<<endl;
 	conf.loadConfig("80_vtx_mf");
+	conf.print();
+	cout<<"==================\n\n";
+
+
+	cout<<"Loading Configuration for Name=76"<<endl;
+	conf.loadConfig("76");
+	conf.print();
+	cout<<"==================\n\n";
+
+	cout<<"==================\n";
+	cout<<"Loading First Configuration"<<endl;
+	string name = conf.findFirstConfig();
+	cout<<"Config is named: "<<name<<endl;
+	if (name=="76"){
+		cout<<"Name is 76"<<endl;
+	}
+	else{
+		cout<<"Name is not 76"<<endl;
+	}
+	conf.loadConfig(name);
+	conf.print();
+	cout<<"==================\n\n";
+
+	cout<<"Loading Configuration for Name=80_vtx_mf"<<endl;
+	conf.loadConfig("80_vtx_mf");
+	conf.print();
+	cout<<"==================\n\n";
+
+	cout<<"Loading Configuration for Name=76"<<endl;
+	conf.loadConfig("76");
 	conf.print();
 	cout<<"==================\n\n";
 
@@ -24,8 +65,42 @@ void test(){
 	conf.loadConfig("NONE"); //should throw error
 	conf.print();
 	cout<<"==================\n\n";
+
+
+	//No breaking after it can't find one
+	cout<<"Loading Configuration for Name=76"<<endl;
+	conf.loadConfig("76");
+	conf.print();
+	cout<<"==================\n\n";
+}
+
+void testSeqAccess(){
+	ConfigParser conf("test.conf");
+	while (conf.loadNextConfig()){
+		cout<<"CONFIG:"<<endl;
+		conf.print();
+	}
+}
+
+void testConfigReset(){
+	ConfigParser conf("test.conf");
+	conf.loadNextConfig();
+	conf.print();
+	conf.loadNextConfig();
+	conf.print();
+	conf.loadConfig("80");
+	conf.print();
+	conf.loadNextConfig();
+	conf.print();
 }
 
 int main(){
-	test();
+	cout<<"Random Access Testing:"<<endl;
+	testRandomAccess();
+
+	cout<<"\n\nSequential Testing:"<<endl;
+	testSeqAccess();
+	
+	cout<<"\n\nReset Testing:"<<endl;
+	testConfigReset();
 }
